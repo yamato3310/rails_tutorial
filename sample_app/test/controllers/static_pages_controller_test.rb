6d3779c4f38b07e1180c4f94db939c2f5a9406e7
yamato0331 <yamatotunasima1@gmail.com>
2019-05-30 22:10:13 +0900

    5.3.1

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
